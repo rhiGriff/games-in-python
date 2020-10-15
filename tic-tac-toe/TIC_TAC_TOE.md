b5e425a4c06a3048fe28fa6649c6289d44f99e0e
Gavin Edwards <gavin.edwards@ons.gov.uk>
2020-10-15 10:53:33 +0100

    Add minting and Makefile. Update linting complaints

--- tic-tac-toe/TIC_TAC_TOE.md
+++ tic-tac-toe/TIC_TAC_TOE.md
@@ -13,3 +13,30 @@ This game is the traditional tic-tac-toe. The board is a 3x3 square like the fol
 Each player will take it in turns to put an `X` or an `O` in the grid. 
 The first person to get 3 of their symbols in a row wins, if neither player gets 3 symbols 
 in a row then it's a draw. 
+
+
+### Install dependencies
+
+To install any Python dependencies, run: 
+
+```bash
+make install
+```
+
+### Tests
+
+To run the tests, including linting, run:
+
+```bash
+make test
+```
+
+
+### Playing the game
+
+To start the game, run:
+
+```bash
+make run
+```
+
--- tic-tac-toe/tic-tac-toe.py
+++ tic-tac-toe/tic-tac-toe.py
@@ -1,17 +1,16 @@
-import sys
+def main():
+    board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    printGrid(board)
 
-BOARD = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-def main(BOARD):
-    printGrid(BOARD)
-
-def printGrid(BOARD):
-    print(" %c | %c | %c " % (BOARD[0],BOARD[1],BOARD[2]))    
-    print("___|___|___")    
-    print(" %c | %c | %c " % (BOARD[3],BOARD[4],BOARD[5]))    
-    print("___|___|___")    
-    print(" %c | %c | %c " % (BOARD[6],BOARD[7],BOARD[8]))    
+def printGrid(board):
+    print(" %c | %c | %c " % (board[0], board[1], board[2]))
+    print("___|___|___")
+    print(" %c | %c | %c " % (board[3], board[4], board[5]))
+    print("___|___|___")
+    print(" %c | %c | %c " % (board[6], board[7], board[8]))
     print("   |   |   ")
 
+
 if __name__ == "__main__":
-    main(BOARD) 
+    main()
