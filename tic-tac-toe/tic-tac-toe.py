dcf66d46109ebc4f870f927c111aa9b26204910f
Gavin Edwards <gavin.edwards@ons.gov.uk>
2020-10-20 08:32:52 +0100

    Add validation on user input and record which player is next

--- tic-tac-toe/tic-tac-toe.py
+++ tic-tac-toe/tic-tac-toe.py
@@ -1,16 +1,45 @@
+from itertools import cycle
 import time
 
 
 def main():
     board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
     welcome()
-    printGrid(board)
-    choice()
+    turn = cycle([1, 2])
+    gameInPlay = True
+
+    while gameInPlay:
+        player = next(turn)
+        if player == 1:
+            letter = "O"
+        else:
+            letter = "X"
+        printGrid(board)
+        move = getPlayerMove(player, board, letter)
+        print(move)
 
 
-def choice():
-    choice = int(input("Plesse enter where you would like to"
-                 "make your move [1-9]\n"))
+def getPlayerMove(player, board, letter):
+    choice = None
+
+    while True:
+        try:
+            choice = int(input(f"Player {player}: Please enter where you would like to"
+                               f" place an {letter} [1-9]\n"))
+        except ValueError:
+            print("You must enter a number between 1 and 9\n")
+            continue
+
+        if not (1 <= choice <= 9):
+            print("Your choice must be between 1 and 9\n")
+            continue
+
+        if not board[choice-1] == " ":
+            print("That position has already been filled.  Please choose another\n")
+            continue
+        else:
+            break
+
     return choice
 
 
@@ -29,7 +58,7 @@ def welcome():
     print("Player1 is: O")
     print("Player2 is: X\n")
     print("Loading. Please wait...\n\n")
-    time.sleep(3)
+    time.sleep(1)
 
 
 if __name__ == "__main__":
